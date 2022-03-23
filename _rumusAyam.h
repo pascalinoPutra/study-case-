09d51952c4152736bce47e2e6d6e25cb4e97db50
pascalino putra <95904768+pascalinoPutra@users.noreply.github.com>
2022-03-24 02:29:57 +0700

    Add files via upload

---
+++ _rumusAyam.h
@@ -0,0 +1,19 @@
+int kali(int a,int b)
+{
+	int hasil;
+	hasil=a*b;
+	return hasil;
+}
+
+int tambah(int a,int b)
+{
+	int hasil;
+	hasil=a+b;
+	return hasil;
+}
+int kurang(int a,int b)
+{
+	int totalHarga;
+	totalHarga=a-b;
+	return totalHarga;
+}
